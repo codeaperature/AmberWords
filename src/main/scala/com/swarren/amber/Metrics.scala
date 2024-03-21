eec1e863653e22eb1cd84ec82088b30fd8a9dbec
Stephan <codeaperature@users.noreply.github.com>
2024-03-20 22:09:19 -0700

    clean up toString for printing

--- src/main/scala/com/swarren/amber/Metrics.scala
+++ src/main/scala/com/swarren/amber/Metrics.scala
@@ -1,5 +1,5 @@
 package com.swarren.amber
 
 case class Metrics(word: String, len: Int, count: Int) {
-   override def toString = s"$word has a length of $len letter(s) and appears $count time(s)."
+   override def toString = s"'$word' has a length of $len letter(s) and appears $count time(s)."
 }
